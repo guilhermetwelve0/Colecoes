d61398e7ba9fb6456e7c2cc6b7a36754c539ad84
guilhermetwelve0 <rlacabral4@gmail.com>
2021-04-26 18:02:08 -0300

    Add project files.

---
+++ Colecoes/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace Colecoes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lista = new System.Windows.Forms.ListBox();
+            this.btnList = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lista
+            // 
+            this.lista.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lista.FormattingEnabled = true;
+            this.lista.ItemHeight = 29;
+            this.lista.Location = new System.Drawing.Point(13, 1);
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(636, 439);
+            this.lista.TabIndex = 0;
+            // 
+            // btnList
+            // 
+            this.btnList.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnList.Location = new System.Drawing.Point(655, 1);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(221, 66);
+            this.btnList.TabIndex = 1;
+            this.btnList.Text = "List";
+            this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(878, 450);
+            this.Controls.Add(this.btnList);
+            this.Controls.Add(this.lista);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Listas Genericas";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lista;
+        private System.Windows.Forms.Button btnList;
+    }
+}
+
---
+++ Colecoes/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Colecoes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+
+            // Array
+            string[] nomes = new string[3];
+            nomes[0] = "Gabriel";
+            nomes[1] = "Arthur";
+            nomes[2] = "Danny";
+             //Lista Generica
+            List<string> nomes2 = new List<string>();
+
+            nomes2.Add("Lucas");
+            nomes2.Add("Beatriz");
+            nomes2.AddRange(nomes);
+
+
+
+            //if (nomes2.Remove("Lucas"))
+            //{
+            //    MessageBox.Show("Lucas removido");
+            //}
+            //else 
+            //{
+            //    MessageBox.Show("Não foi possível remover.");
+            //}
+
+            //if(nomes2.Contains("Danny"))
+            //{
+            //    MessageBox.Show("Contém");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("Não contém");
+            //}
+
+            //MessageBox.Show("Número de elementos: " + nomes2.Count);
+
+            //nomes2.Sort();
+
+            //MessageBox.Show("Gabriel está no índice: " + nomes2.IndexOf("João"));
+
+            //nomes2.Insert(2, "João");
+
+            //nomes2.RemoveAt(1);
+
+            //nomes2.Clear();
+
+            nomes2.Add("Arthur");
+
+            foreach(string nome in nomes2)
+            {
+                lista.Items.Add(nome);   
+            }
+
+        }
+    }
+}
